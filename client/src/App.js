88dba7eb93638ebca7304aa6891b8d45969cdad0
aliamanatali <aliamanatali218@gmail.com>
2023-10-28 00:48:49 +0500

    Added Client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Createpost from './components/createpost';
+
+function App() {
+  return (
+    <div className="container-fluid App">
+        <Createpost />
+    </div>
+  );
+}
+
+export default App;
